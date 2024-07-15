3acbce6d078392243b60d49a2cbe480009cf687a
2bit-hash <gowasalmon@yahoo.com>
2024-07-15 13:47:37 +0300

    routing

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request,"main.html")
+
+def room(request):
+    return render(request,"room.html")
